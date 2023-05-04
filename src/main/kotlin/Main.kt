6654d4b09b2528e0257d56621b4822a0ebd225e2
anna <hannayanna@protonmail.com>
2023-05-04 15:45:25 +1000

    commit 0.0.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import questions.help
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    when (readln()) {
+        "help" -> help()
+    }
+
 }
+var i = 0
---
+++ src/main/kotlin/questions/help.kt
@@ -0,0 +1,31 @@
+package questions
+
+import i
+
+fun help(){
+    val CartesianPlane = arrayOf("length","Midpoint","gradient(slope)")
+
+
+    println("----------------------------------------------")
+    println("WHAT TOPIC DO YOU NEED HELP WITH?")
+    println("----------------------------------------------")
+    println("cartesian plane?")
+    when (readln()) {
+        "cartesian plane" -> helpMaker("Cartesian Plane", CartesianPlane)
+    }
+
+}
+private fun helpMaker(groupName: String,array: Array<String>) {
+    println("----------------------------------------------")
+    println(groupName.uppercase())
+    println("----------------------------------------------")
+    arrayPrinter(array)
+    println("----------------------------------------------")
+}
+private fun arrayPrinter( array: Array<String>){
+    while (i != array.size) {
+        println(array[i])
+        i++
+    }
+    i = 0
+}
